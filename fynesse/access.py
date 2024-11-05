dda47bf9541028a37d34d76583f5c278b41f70b1
MatthewGregson123 <159264069+MatthewGregson123@users.noreply.github.com>
2024-11-05 15:07:36 +0000

    Update access.py

--- fynesse/access.py
+++ fynesse/access.py
@@ -16,14 +16,14 @@ def fynesse_download_price_paid_data(year_from, year_to):
     base_url = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com"
     """Download UK house price data for given year range"""
     # File name with placeholders
-    file_name = "/pp--part.csv"
+    file_name = "/pp-<year>-part<part>.csv"
     for year in range(year_from, (year_to+1)):
         print (f"Downloading data for year: {year}")
         for part in range(1,3):
-            url = base_url + file_name.replace("", str(year)).replace("", str(part))
+            url = base_url + file_name.replace("<year>", str(year)).replace("<part>", str(part))
             response = requests.get(url)
             if response.status_code == 200:
-                with open("." + file_name.replace("", str(year)).replace("", str(part)), "wb") as file:
+                with open("." + file_name.replace("<year>", str(year)).replace("<part>", str(part)), "wb") as file:
                     file.write(response.content)
 
 def data():
