bbcfbc0ebbe506c6f8420e1fa216958f8c3bba50
MatthewGregson123 <159264069+MatthewGregson123@users.noreply.github.com>
2024-11-06 17:36:27 +0000

    Added create_connection function to access.py

--- fynesse/access.py
+++ fynesse/access.py
@@ -40,7 +40,30 @@ def download_open_postcode_geo_data():
         file_name = "/open_postcode_geo"
         with zipfile.ZipFile("." + zip_file_name, "r") as file:            
             file.extractall("." + file_name)
-        
+            
+def create_connection(user, password, host, database, port=3306):
+    """ Create a database connection to the MariaDB database
+        specified by the host url and database name.
+    :param user: username
+    :param password: password
+    :param host: host url
+    :param database: database name
+    :param port: port number
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = pymysql.connect(user=user,
+                               passwd=password,
+                               host=host,
+                               port=port,
+                               local_infile=1,
+                               db=database
+                               )
+        print(f"Connection established!")
+    except Exception as e:
+        print(f"Error connecting to the MariaDB Server: {e}")
+    return conn
 
 def data():
     """Read the data from the web or local file, returning structured format such as a data frame"""
