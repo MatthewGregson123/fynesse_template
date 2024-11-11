1a6f1af1bc7d26424962c9ff5450dd2b873bb497
MatthewGregson123 <159264069+MatthewGregson123@users.noreply.github.com>
2024-11-11 17:32:44 +0000

    Update assess.py
    added import math

--- fynesse/assess.py
+++ fynesse/assess.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 import osmnx as ox
 import scipy.stats as stats
 import numpy as np
+import math
 
 """Place commands in this file to assess the data you have downloaded. How are missing values encoded, how are outliers encoded? What do columns represent, makes rure they are correctly labeled. How is the data indexed. Crete visualisation routines to assess the data (e.g. in bokeh). Ensure that date formats are correct and correctly timezoned."""
 
