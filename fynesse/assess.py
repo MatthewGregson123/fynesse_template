3034f555441cb1741c9e08d39891f039061ba2c7
MatthewGregson123 <159264069+MatthewGregson123@users.noreply.github.com>
2024-11-11 17:28:45 +0000

    Update assess.py
    adjusted plot map to take in north south west east

--- fynesse/assess.py
+++ fynesse/assess.py
@@ -16,7 +16,7 @@ import numpy as np
 
 """Place commands in this file to assess the data you have downloaded. How are missing values encoded, how are outliers encoded? What do columns represent, makes rure they are correctly labeled. How is the data indexed. Crete visualisation routines to assess the data (e.g. in bokeh). Ensure that date formats are correct and correctly timezoned."""
 
-def plot_map(area,edges, buildings_with_addresses_pois, buildings_without_addresses_pois):
+def plot_map(area,edges, buildings_with_addresses_pois, buildings_without_addresses_pois, north, south, west, east):
   fig, ax = plt.subplots()
 
   # Plot the footprint
