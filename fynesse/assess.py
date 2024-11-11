dd336cbb56d40329365c5d3a505d8bd3c5cf6f09
MatthewGregson123 <159264069+MatthewGregson123@users.noreply.github.com>
2024-11-11 21:28:31 +0000

    added import pandas to assess.py

--- fynesse/assess.py
+++ fynesse/assess.py
@@ -16,6 +16,7 @@ import numpy as np
 import math
 import seaborn as sns
 import random
+import pandas as pd
 
 """Place commands in this file to assess the data you have downloaded. How are missing values encoded, how are outliers encoded? What do columns represent, makes rure they are correctly labeled. How is the data indexed. Crete visualisation routines to assess the data (e.g. in bokeh). Ensure that date formats are correct and correctly timezoned."""
 
