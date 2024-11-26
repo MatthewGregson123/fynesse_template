bfae045d18853775f43658997977795490dfc39d
Matthew Gregson <159264069+MatthewGregson123@users.noreply.github.com>
2024-11-26 14:02:57 +0000

    Update access.py
    added functionality for downloading census data

--- fynesse/access.py
+++ fynesse/access.py
@@ -248,6 +248,26 @@ def combine_data(prices_coordinates_data_result, addresses_pois_df):
   price_area_df = pd.DataFrame(matches, columns=columns)
   return price_area_df
 
+def download_census_data(code, base_dir=''):
+  url = f'https://www.nomisweb.co.uk/output/census/2021/census2021-{code.lower()}.zip'
+  extract_dir = os.path.join(base_dir, os.path.splitext(os.path.basename(url))[0])
+
+  if os.path.exists(extract_dir) and os.listdir(extract_dir):
+    print(f"Files already exist at: {extract_dir}.")
+    return
+
+  os.makedirs(extract_dir, exist_ok=True)
+  response = requests.get(url)
+  response.raise_for_status()
+
+  with zipfile.ZipFile(io.BytesIO(response.content)) as zip_ref:
+    zip_ref.extractall(extract_dir)
+
+  print(f"Files extracted to: {extract_dir}")
+
+def load_census_data(code, level='msoa'):
+  return pd.read_csv(f'census2021-{code.lower()}/census2021-{code.lower()}-{level}.csv')
+
 def data():
     """Read the data from the web or local file, returning structured format such as a data frame"""
     raise NotImplementedError
