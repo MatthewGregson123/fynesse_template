983ea685f1c57a303be4ccd3c8426d235d442968
Matthew Gregson <159264069+MatthewGregson123@users.noreply.github.com>
2024-11-27 22:46:03 +0000

    Update access.py
    import math

--- fynesse/access.py
+++ fynesse/access.py
@@ -14,6 +14,7 @@ import time
 import osmnx as ox
 import pandas as pd
 import io
+import math
 # This file accesses the data
 
 """Place commands in this file to access the data electronically. Don't remove any missing values, or deal with outliers. Make sure you have legalities correct, both intellectual property and personal data privacy rights. Beyond the legal side also think about the ethical issues around this data. """
