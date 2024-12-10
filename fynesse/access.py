b8778adaa2b8df3961e768016b683515b4628e2e
Matthew Gregson <159264069+MatthewGregson123@users.noreply.github.com>
2024-12-10 15:51:06 +0000

    Update access.py

--- fynesse/access.py
+++ fynesse/access.py
@@ -18,7 +18,7 @@ import math
 import ijson
 import osmium
 from shapely import wkt
-from shapely.geometry import shape, Polygon, MultiPolygon
+from shapely.geometry import shape, Polygon, MultiPolygon, LineString, Point
 # This file accesses the data
 
 """Place commands in this file to access the data electronically. Don't remove any missing values, or deal with outliers. Make sure you have legalities correct, both intellectual property and personal data privacy rights. Beyond the legal side also think about the ethical issues around this data. """
