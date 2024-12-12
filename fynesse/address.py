ffb0f3ba87a7db302bcc8bdda9a30323494c2a90
Matthew Gregson <159264069+MatthewGregson123@users.noreply.github.com>
2024-12-12 22:45:48 +0000

    Update address.py

--- fynesse/address.py
+++ fynesse/address.py
@@ -134,4 +134,88 @@ def get_population_density(lat, long, cursor):
   total_density = np.sum(density_data)
 
   return total_density
+
+def create_train_data_L15_pop_density(locations_dict, username, password, url):
+  x_train = []
+  x_train_density = []
+  y_train = []
+  y_train_density = []
+
+  conn = fynesse.access.create_connection(username, password, url, database='ads_2024')
+  cursor = conn.cursor()
+
+
+  query = f"SELECT LAT, `LONG` FROM geo_coords_data"
+  cursor.execute(query)
+
+  c = 0
+  target = 100
+
+  indexes = []
+
+  row = cursor.fetchall()
+  i = 0
+  j = 0
+
+  must_examine = [(51.5189, -0.09083), (51.519, -0.09649),(51.5138, -0.09959), (51.5171, -0.09361), (51.5208, -0.09625)]
+  while c < target or i < len(locations_dict) or j < len(must_examine):
+    print(c, end=', ')
+    if c < target:
+      index = random.randint(0, len(row)-1)
+      while index in indexes:
+        index = random.randint(0, len(row)-1)
+      indexes.append(index)
+      lat, long = row[index]
+    elif i < len(locations_dict):
+      lat, long = locations_dict[list(locations_dict.keys())[i]]
+      i += 1
+    else:
+      lat, long = must_examine[j]
+      j += 1
+    data = True
+    with warnings.catch_warnings():
+      warnings.simplefilter("ignore")
+      try:
+        poi_counts_df = fynesse.assess.get_pois_counts_from_sql({"location": (lat, long)}, cursor)
+      except:
+        data = False
+        continue
+    if data:
+      c += 1
+      norm_poi_counts_df = poi_counts_df.div(poi_counts_df.sum(axis=1), axis=0)
+
+
+      nsec_df = fynesse.assess.get_nsec_df_for_locations({"location": (lat, long)}, cursor)
+      nsec_df_noL15 = nsec_df.drop('L15',axis=1)
+      norm_nsec_df = nsec_df.div(nsec_df.sum(axis=1), axis=0)
+      norm_nsec_df_noL15 = nsec_df_noL15.div(nsec_df_noL15.sum(axis=1), axis=0)
+
+      norm_nsec_poi_counts_df = pd.concat([norm_nsec_df, norm_poi_counts_df], axis=1)
+      norm_nsec_poi_counts_df_noL15 = pd.concat([norm_nsec_df_noL15, norm_poi_counts_df], axis=1)
+
+      train_row = norm_nsec_poi_counts_df_noL15.to_numpy()
+      train_row = train_row[0]
+      train_row = np.nan_to_num(train_row, nan=0)
+
+      x_train.append(train_row)
+      y_train.append(norm_nsec_poi_counts_df['L15'])
+
+      density = get_population_density(lat, long, cursor)
+      y_train_density.append(density)
+
+      # train_row = norm_nsec_poi_counts_df.to_numpy()
+      train_row = pd.concat([nsec_df, poi_counts_df], axis=1).to_numpy()
+      train_row = train_row[0]
+      train_row = np.nan_to_num(train_row, nan=0)
+
+      x_train_density.append(train_row)
+
+
+  x_train = np.array(x_train)
+  x_train_density = np.array(x_train_density)
+
+  y_train = np.array(y_train)
+  y_train_density = np.array(y_train_density)
+
+  return x_train, y_train, x_train_density, y_train_density
   
