96b8e4fc1e19a24c4909179763e87111e2084e39
Matthew Gregson <159264069+MatthewGregson123@users.noreply.github.com>
2024-12-13 02:10:10 +0000

    Update assess.py

--- fynesse/assess.py
+++ fynesse/assess.py
@@ -18,6 +18,8 @@ import seaborn as sns
 import random
 from sklearn import decomposition
 import pandas as pd
+import geopandas as gpd
+from shapely.wkt import loads, dumps
 
 """Place commands in this file to assess the data you have downloaded. How are missing values encoded, how are outliers encoded? What do columns represent, makes rure they are correctly labeled. How is the data indexed. Crete visualisation routines to assess the data (e.g. in bokeh). Ensure that date formats are correct and correctly timezoned."""
 
